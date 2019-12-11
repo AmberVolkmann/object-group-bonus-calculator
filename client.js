6f1de7d7a77f56fd9c9964039aa03464297e0dd0
Nic Payne <nic.r.payne@gmail.com>
2019-12-11 10:32:35 -0600

    no erros - crab salad

--- client.js
+++ client.js
@@ -40,4 +40,77 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+
+
+function createNew(employee) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(employees[i]);
+    
+    let bonusPercentage = 0;
+
+    let totalBonus = employees.annualSalary * bonusPercentage;
+
+    let totalCompensation = employees.annualSalary + totalBonus;
+    
+    let newEmployee = {
+      name: employees.name,
+      bonusPercentage: bonusPercentage,
+      totalCompensation: totalCompensation,
+      totalBonus: totalBonus
+    } 
+    console.log(newEmployee);
+  }
+
+}
+
+console.log(createNew (employees));
+
+
+// function grabEmployee() {
+//   const newEmployeeList = [];
+//   for (let employee of employees) {
+//     if (employees.reviewRating <= 2) {
+//       newEmployeeList.ratingCalc.push();
+//     } else if (employees.employeenumber === 4) {
+//       newEmployeeList.employeeNumb.push();
+//     } else if (employees.annualSalary > 65000) {
+//       newEmployeeList.smallSalary.push();
+//     } else if (reviewRating === 5) {
+//       bonusPercentage = 1;
+//     }
+
+//   }
+
+
+
+function bonusCalc(employee) {
+  console.log('In bonusCalc');
+  let bonusPercentage = 0;
+  // let bonusPercentage = 0;
+  // let totalCompensation = employee.annualSalary + totalBonus;
+  // let totalBonus = employee.annualSalary * bonusPercentage;
+
+  if (employees.reviewRating <= 2) {
+    bonusPercentage = 0;
+  } else if (employees.reviewRating === 3) {
+    bonusPercentage = .04;
+  } else if (employees.reviewRating === 4) {
+    bonusPercentage = .06;
+  } else if (employees.reviewRating === 5) {
+    bonusPercentage = 1;
+  }
+  if (employees.employeeNumber <= 9999) {
+    bonusPercentage += .05;
+  }
+  if (employees.annualSalary > 65000) {
+    bonusPercentage -= .01;
+  } createNew();
+  return bonusPercentage;
+}
+
+
+
+
+console.log(employees);
+bonusCalc();
+createNew();
